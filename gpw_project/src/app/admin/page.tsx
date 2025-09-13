de01f9473b6a1e4a5a9737da42b4354fe7a6fd28
Fanzzxly <syafiqardyrifanza@gmail.com>
2025-09-13 17:23:56 +0900

    Build Fondasi: Home,Admin,Login

---
+++ gpw_project/src/app/admin/page.tsx
@@ -0,0 +1,7 @@
+export default function AdminPage() {
+  return (
+    <main className="min-h-dvh grid place-items-center">
+      <h1 className="text-2xl font-semibold">Admin</h1>
+    </main>
+  );
+}
---
+++ gpw_project/src/app/login/page.tsx
@@ -0,0 +1,7 @@
+export default function LoginPage() {
+  return (
+    <main className="min-h-dvh grid place-items-center">
+      <h1 className="text-2xl font-semibold">Login</h1>
+    </main>
+  );
+}
